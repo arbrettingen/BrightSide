785115f8fcf44b99b39322dbdcd9cc2246bfcf03
abrettingen <arbrettingen@gmail.com>
2017-03-30 11:01:14 -0700

    Added initial class vars, G&S methods to Aff class

--- app/src/main/java/com/soapbox/brightside/Affirmation.java
+++ app/src/main/java/com/soapbox/brightside/Affirmation.java
@@ -5,4 +5,28 @@ package com.soapbox.brightside;
  */
 
 public class Affirmation {
+
+    private String mImgSrc;
+    private String mAffirmationBody;
+
+    public Affirmation(String imgSrc, String affirmationBody){
+        mImgSrc = imgSrc;
+        mAffirmationBody = affirmationBody;
+    }
+
+    public String getmImgSrc() {
+        return mImgSrc;
+    }
+
+    public void setmImgSrc(String mImgSrc) {
+        this.mImgSrc = mImgSrc;
+    }
+
+    public String getmAffirmationBody() {
+        return mAffirmationBody;
+    }
+
+    public void setmAffirmationBody(String mAffirmationBody) {
+        this.mAffirmationBody = mAffirmationBody;
+    }
 }
