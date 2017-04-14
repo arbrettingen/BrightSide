57d98bd98b6a07b90e5b7dabae6a0a562573a9e0
abrettingen <arbrettingen@gmail.com>
2017-04-13 23:06:19 -0700

    Added ability for user to add custom affirmation to master list.

--- app/src/main/java/com/soapbox/brightside/NewAffirmationActivity.java
+++ app/src/main/java/com/soapbox/brightside/NewAffirmationActivity.java
@@ -1,16 +1,22 @@
 package com.soapbox.brightside;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Alex on 4/12/2017.
@@ -23,6 +29,9 @@ public class NewAffirmationActivity extends AppCompatActivity {
     private ListView mDrawerList;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+
+    private Button mAddNewAffirmation;
+    private EditText mAffirmationEdit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,6 +68,43 @@ public class NewAffirmationActivity extends AppCompatActivity {
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //add new button and edit text setup below
+        mAffirmationEdit = (EditText) findViewById(R.id.new_affirmation_edit);
+
+        mAddNewAffirmation = (Button) findViewById(R.id.btn_add_affirmation_confirm);
+        mAddNewAffirmation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mAffirmationEdit.getText().toString().equals("") || mAffirmationEdit.getText().toString().equals("Enter affirmation text here")) {
+                    Toast.makeText(getApplicationContext(), "Affirmation text is invalid.", Toast.LENGTH_LONG).show();
+                } else {
+
+                    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case DialogInterface.BUTTON_POSITIVE:
+                                    MainMenuActivity.mMasterAffirmationList.add(new Affirmation(mAffirmationEdit.getText().toString()));
+                                    dialog.dismiss();
+                                    Toast.makeText(getApplicationContext(), "Affirmation successfully added", Toast.LENGTH_LONG).show();
+                                    mAffirmationEdit.setText("");
+                                    break;
+
+                                case DialogInterface.BUTTON_NEGATIVE:
+                                    dialog.dismiss();
+                                    break;
+                            }
+                        }
+                    };
+
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(NewAffirmationActivity.this, R.style.myDialog));
+                    builder.setMessage("Are you sure you'd like to add this affirmation?").setPositiveButton("Yes", dialogClickListener).setNegativeButton("No", dialogClickListener).show();
+                }
+            }
+        });
 
     }
 
